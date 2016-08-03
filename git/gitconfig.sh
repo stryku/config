051170e24da5347abe4187db3b9e455b6839928b
stryku <stryku2393@gmail.com>
2016-08-03 10:38:35 +0200

    added git aliases configs

---
+++ git/gitconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global alias.cm 'commit -v -m'
+git config --global alias.co 'checkout'
+git config --global alias.cb 'checkout -b'
+git config --global alias.aa 'add --all .'
+git config --global alias.st 'status'
+git config --global alias.last 'log -1 HEAD'
