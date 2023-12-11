1b1d817255cbbc771e3d1232d07dc341ff0d06d8
stryku <stryku2393@gmail.com>
2023-12-11 07:00:08 +0100

    clang

--- apt-install.sh
+++ apt-install.sh
@@ -13,6 +13,14 @@ sudo apt install -y \
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+figlet clang
+mkdir /tmp/clang-install
+cd /tmp/clang-install
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 17
+cd -
+
 figlet manual
 echo "VS code: https://code.visualstudio.com/download"
 echo "double commander: ubuntu sofrware"
